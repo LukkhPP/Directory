4944f0e6813428acd8a8066644281d0fdfc6c4d5
LukkhPP <yugiinari18@gmail.com>
2024-12-02 15:26:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-import * as THREE from 'three';
+/*import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';*/
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.154.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/controls/OrbitControls.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+
 import { locationxyz } from './functions.js';
 import { samename } from './functions.js';
 
